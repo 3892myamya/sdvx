3c0ebaae1404383df744fd94671879553ac93ca3
3892myamya <going3892way@hotmail.com>
2019-01-06 22:20:05 +0900

    ・統計情報を火曜日・金曜日の初回実行時に自動更新する機能を実装

---
+++ src/main/java/myamya/sdvx/KadaiGeneratorUtil.java
@@ -0,0 +1,29 @@
+package myamya.sdvx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * 便利メソッドをまとめたクラスです。
+ */
+public class KadaiGeneratorUtil {
+
+	private KadaiGeneratorUtil() {
+		// インスタンス生成禁止
+	}
+
+	/**
+	 * urlのリーダーを取得します。
+	 */
+	public static BufferedReader getReader(URL url) throws IOException, ProtocolException {
+		HttpURLConnection http = (HttpURLConnection) url.openConnection();
+		http.setRequestMethod("GET");
+		http.connect();
+		return new BufferedReader(new InputStreamReader(http.getInputStream(), "UTF-8"));
+	}
+
+}
