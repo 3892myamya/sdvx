808edbf8daf6bd48ac1829d086e05648147d7c5a
3892myamya <3892myamya@gmail.com>
2019-07-27 23:16:44 +0900

    ヤジリン少しだけ強化

--- src/main/java/myamya/other/solver/yajilin/YajilinSolver.java
+++ src/main/java/myamya/other/solver/yajilin/YajilinSolver.java
@@ -921,31 +921,26 @@ public class YajilinSolver implements Solver {
 		}
 
 		/**
-		 * 白マスが1つながりになっていない場合falseを返す。
+		 * 白マスがひとつながりにならない場合Falseを返す。
 		 */
 		public boolean connectSolve() {
 			Set<Position> whitePosSet = new HashSet<>();
-			Position typicalWhitePos = null;
 			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
 				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
 					if (masu[yIndex][xIndex] == MasuImpl.NOT_BLACK) {
 						Position whitePos = new Position(yIndex, xIndex);
-						whitePosSet.add(whitePos);
-						if (typicalWhitePos == null) {
-							typicalWhitePos = whitePos;
+						if (whitePosSet.isEmpty()) {
+							whitePosSet.add(whitePos);
+							setContinuePosSet(whitePos, whitePosSet, null);
+						} else {
+							if (!whitePosSet.contains(whitePos)) {
+								return false;
+							}
 						}
 					}
 				}
 			}
-			if (typicalWhitePos == null) {
-				return true;
-			} else {
-				Set<Position> continuePosSet = new HashSet<>();
-				continuePosSet.add(typicalWhitePos);
-				setContinuePosSet(typicalWhitePos, continuePosSet, null);
-				whitePosSet.removeAll(continuePosSet);
-				return whitePosSet.isEmpty();
-			}
+			return true;
 		}
 
 		/**
