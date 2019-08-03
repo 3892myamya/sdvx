634900f43a9c08e7235ab07008a6b9826240b140
3892myamya <3892myamya@gmail.com>
2019-08-03 22:30:48 +0900

    数コロ

---
+++ src/main/java/myamya/other/solver/sukoro/SukoroSolver.java
@@ -0,0 +1,445 @@
+package myamya.other.solver.sukoro;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import myamya.other.solver.Common.Difficulty;
+import myamya.other.solver.Common.Direction;
+import myamya.other.solver.Common.Position;
+import myamya.other.solver.Solver;
+
+public class SukoroSolver implements Solver {
+
+	public static class Field {
+		static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+		// 表出数字
+		private final Integer[][] numbers;
+		// 数字の候補情報
+		protected List<Integer>[][] numbersCand;
+
+		public List<Integer>[][] getNumbersCand() {
+			return numbersCand;
+		}
+
+		public Integer[][] getArrows() {
+			return numbers;
+		}
+
+		public int getYLength() {
+			return numbersCand.length;
+		}
+
+		public int getXLength() {
+			return numbersCand[0].length;
+		}
+
+		@SuppressWarnings("unchecked")
+		public Field(int height, int width, String param) {
+			numbers = new Integer[height][width];
+			numbersCand = new ArrayList[height][width];
+			// 0(空白)から4まで候補を決定
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					numbersCand[yIndex][xIndex] = new ArrayList<>();
+					for (int number = 0; number <= 4; number++) {
+						numbersCand[yIndex][xIndex].add(number);
+					}
+				}
+			}
+			int index = 0;
+			for (int i = 0; i < param.length(); i++) {
+				char ch = param.charAt(i);
+				int interval = ALPHABET.indexOf(ch);
+				if (interval != -1) {
+					index = index + interval + 1;
+				} else {
+					//16 - 255は '-'
+					//256 - 999は '+'
+					int num;
+					if (ch == '.') {
+						//
+					} else {
+						if (ch == '-') {
+							num = Integer.parseInt("" + param.charAt(i + 1) + param.charAt(i + 2), 16);
+							i++;
+							i++;
+						} else if (ch == '+') {
+							num = Integer.parseInt(
+									"" + param.charAt(i + 1) + param.charAt(i + 2) + param.charAt(i + 3),
+									16);
+							i++;
+							i++;
+							i++;
+						} else {
+							num = Integer.parseInt(String.valueOf(ch), 16);
+						}
+						Position pos = new Position(index / getXLength(), index % getXLength());
+						numbersCand[pos.getyIndex()][pos.getxIndex()] = new ArrayList<>();
+						numbersCand[pos.getyIndex()][pos.getxIndex()].add(num);
+						numbers[pos.getyIndex()][pos.getxIndex()] = num;
+					}
+					index++;
+				}
+			}
+
+		}
+
+		@SuppressWarnings("unchecked")
+		public Field(Field other) {
+			numbers = other.numbers;
+			numbersCand = new ArrayList[other.getYLength()][other.getXLength()];
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					numbersCand[yIndex][xIndex] = new ArrayList<>(other.numbersCand[yIndex][xIndex]);
+				}
+			}
+		}
+
+		private static final String HALF_NUMS = "0 1 2 3 4 5 6 7 8 9";
+		private static final String FULL_NUMS = "０１２３４５６７８９";
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					if (numbersCand[yIndex][xIndex].size() == 0) {
+						sb.append("×");
+					} else if (numbersCand[yIndex][xIndex].size() == 1) {
+						String numStr = String.valueOf(numbersCand[yIndex][xIndex].get(0));
+						int index = HALF_NUMS.indexOf(numStr);
+						if (index == 0) {
+							sb.append("・");
+						} else {
+							sb.append(FULL_NUMS.substring(index / 2, index / 2 + 1));
+						}
+					} else if (numbersCand[yIndex][xIndex].size() == 2) {
+						sb.append(numbersCand[yIndex][xIndex].get(0));
+						sb.append(numbersCand[yIndex][xIndex].get(1));
+					} else {
+						sb.append("　");
+					}
+				}
+				sb.append(System.lineSeparator());
+			}
+			return sb.toString();
+		}
+
+		public String getStateDump() {
+			StringBuilder sb = new StringBuilder();
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					sb.append(numbersCand[yIndex][xIndex].size());
+				}
+			}
+			return sb.toString();
+		}
+
+		/**
+		 * 各種チェックを1セット実行
+		 */
+		private boolean solveAndCheck() {
+			String str = getStateDump();
+			if (!numberSolve()) {
+				return false;
+			}
+			if (!getStateDump().equals(str)) {
+				return solveAndCheck();
+			} else {
+				if (!connectSolve()) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		/**
+		 * 数字が0以外の場合、自分の周りにある0以外の数は数字と同じになる。
+		 * また、同じ数字は隣り合わない。
+		 */
+		private boolean numberSolve() {
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					int numberCount = 0;
+					int zeroCount = 0;
+					List<Integer> upCands = yIndex == 0 ? null : numbersCand[yIndex - 1][xIndex];
+					List<Integer> rightCands = xIndex == getXLength() - 1 ? null
+							: numbersCand[yIndex][xIndex + 1];
+					List<Integer> downCands = yIndex == getYLength() - 1 ? null
+							: numbersCand[yIndex + 1][xIndex];
+					List<Integer> leftCands = xIndex == 0 ? null : numbersCand[yIndex][xIndex - 1];
+					if (upCands != null && !upCands.contains(0)) {
+						numberCount++;
+					} else if (upCands == null || (upCands.size() == 1 && upCands.get(0) == 0)) {
+						zeroCount++;
+					}
+					if (rightCands != null && !rightCands.contains(0)) {
+						numberCount++;
+					} else if (rightCands == null || (rightCands.size() == 1 && rightCands.get(0) == 0)) {
+						zeroCount++;
+					}
+					if (downCands != null && !downCands.contains(0)) {
+						numberCount++;
+					} else if (downCands == null || (downCands.size() == 1 && downCands.get(0) == 0)) {
+						zeroCount++;
+					}
+					if (leftCands != null && !leftCands.contains(0)) {
+						numberCount++;
+					} else if (leftCands == null || (leftCands.size() == 1 && leftCands.get(0) == 0)) {
+						zeroCount++;
+					}
+					for (Iterator<Integer> iterator = numbersCand[yIndex][xIndex].iterator(); iterator.hasNext();) {
+						int number = iterator.next();
+						if (number != 0 && (number < numberCount || number > 4 - zeroCount)) {
+							// 0以外で、自分の数字が大きすぎる、または小さすぎる候補を消す。
+							iterator.remove();
+						}
+					}
+					if (numbersCand[yIndex][xIndex].isEmpty()) {
+						return false;
+					}
+					if (numbersCand[yIndex][xIndex].size() == 1) {
+						Integer number = numbersCand[yIndex][xIndex].get(0);
+						if (number == 0) {
+							continue;
+						}
+						// 同じ数字は隣り合わない
+						if (upCands != null) {
+							upCands.remove(number);
+							if (upCands.isEmpty()) {
+								return false;
+							}
+						}
+						if (rightCands != null) {
+							rightCands.remove(number);
+							if (rightCands.isEmpty()) {
+								return false;
+							}
+						}
+						if (downCands != null) {
+							downCands.remove(number);
+							if (downCands.isEmpty()) {
+								return false;
+							}
+						}
+						if (leftCands != null) {
+							leftCands.remove(number);
+							if (leftCands.isEmpty()) {
+								return false;
+							}
+						}
+						// 自分が0以外の数字に確定している場合、周囲の候補を狭める
+						if (number == numberCount) {
+							// 数字の数が等しいことが分かれば、それ以外のマスは0確定
+							if (upCands != null && upCands.size() > 1 && upCands.contains(0)) {
+								upCands.clear();
+								upCands.add(0);
+							}
+							if (rightCands != null && rightCands.size() > 1 && rightCands.contains(0)) {
+								rightCands.clear();
+								rightCands.add(0);
+							}
+							if (downCands != null && downCands.size() > 1 && downCands.contains(0)) {
+								downCands.clear();
+								downCands.add(0);
+							}
+							if (leftCands != null && leftCands.size() > 1 && leftCands.contains(0)) {
+								leftCands.clear();
+								leftCands.add(0);
+							}
+						}
+						if (number == 4 - zeroCount) {
+							// 0の数が等しいことが分かれば、それ以外のマスは0以外確定
+							if (upCands != null && upCands.size() > 1 && upCands.contains(0)) {
+								upCands.remove(new Integer(0));
+							}
+							if (rightCands != null && rightCands.size() > 1 && rightCands.contains(0)) {
+								rightCands.remove(new Integer(0));
+							}
+							if (downCands != null && downCands.size() > 1 && downCands.contains(0)) {
+								downCands.remove(new Integer(0));
+							}
+							if (leftCands != null && leftCands.size() > 1 && leftCands.contains(0)) {
+								leftCands.remove(new Integer(0));
+							}
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		/**
+		 * 数字マスがひとつながりにならない場合Falseを返す。
+		 */
+		public boolean connectSolve() {
+			Set<Position> numberPosSet = new HashSet<>();
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					if (numbersCand[yIndex][xIndex].size() == 1 && numbersCand[yIndex][xIndex].get(0) != 0) {
+						Position numberPos = new Position(yIndex, xIndex);
+						if (numberPosSet.size() == 0) {
+							numberPosSet.add(numberPos);
+							setContinuePosSet(numberPos, numberPosSet, null);
+						} else {
+							if (!numberPosSet.contains(numberPos)) {
+								return false;
+							}
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		/**
+		 * posを起点に上下左右に0確定でないマスを無制限につなげていく。
+		 */
+		private void setContinuePosSet(Position pos, Set<Position> continuePosSet, Direction from) {
+			if (pos.getyIndex() != 0 && from != Direction.UP) {
+				Position nextPos = new Position(pos.getyIndex() - 1, pos.getxIndex());
+				if (!continuePosSet.contains(nextPos)
+						&& !(numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].size() == 1 &&
+								numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].get(0) == 0)) {
+					continuePosSet.add(nextPos);
+					setContinuePosSet(nextPos, continuePosSet, Direction.DOWN);
+				}
+			}
+			if (pos.getxIndex() != getXLength() - 1 && from != Direction.RIGHT) {
+				Position nextPos = new Position(pos.getyIndex(), pos.getxIndex() + 1);
+				if (!continuePosSet.contains(nextPos)
+						&& !(numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].size() == 1 &&
+								numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].get(0) == 0)) {
+					continuePosSet.add(nextPos);
+					setContinuePosSet(nextPos, continuePosSet, Direction.LEFT);
+				}
+			}
+			if (pos.getyIndex() != getYLength() - 1 && from != Direction.DOWN) {
+				Position nextPos = new Position(pos.getyIndex() + 1, pos.getxIndex());
+				if (!continuePosSet.contains(nextPos)
+						&& !(numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].size() == 1 &&
+								numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].get(0) == 0)) {
+					continuePosSet.add(nextPos);
+					setContinuePosSet(nextPos, continuePosSet, Direction.UP);
+				}
+			}
+			if (pos.getxIndex() != 0 && from != Direction.LEFT) {
+				Position nextPos = new Position(pos.getyIndex(), pos.getxIndex() - 1);
+				if (!continuePosSet.contains(nextPos)
+						&& !(numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].size() == 1 &&
+								numbersCand[nextPos.getyIndex()][nextPos.getxIndex()].get(0) == 0)) {
+					continuePosSet.add(nextPos);
+					setContinuePosSet(nextPos, continuePosSet, Direction.RIGHT);
+				}
+			}
+		}
+
+		public boolean isSolved() {
+			for (int yIndex = 0; yIndex < getYLength(); yIndex++) {
+				for (int xIndex = 0; xIndex < getXLength(); xIndex++) {
+					if (numbersCand[yIndex][xIndex].size() != 1) {
+						return false;
+					}
+				}
+			}
+			return solveAndCheck();
+		}
+
+	}
+
+	private final Field field;
+	private int count = 0;
+
+	public SukoroSolver(int height, int width, String param) {
+		field = new Field(height, width, param);
+	}
+
+	public Field getField() {
+		return field;
+	}
+
+	public static void main(String[] args) {
+		String url = "";//urlを入れれば試せる
+		String[] params = url.split("/");
+		int height = Integer.parseInt(params[params.length - 2]);
+		int width = Integer.parseInt(params[params.length - 3]);
+		String param = params[params.length - 1];
+		System.out.println(new SukoroSolver(height, width, param).solve());
+	}
+
+	@Override
+	public String solve() {
+		long start = System.nanoTime();
+		while (!field.isSolved()) {
+			System.out.println(field);
+			String befStr = field.getStateDump();
+			if (!field.solveAndCheck()) {
+				System.out.println(field);
+				return "問題に矛盾がある可能性があります。途中経過を返します。";
+			}
+			if (field.getStateDump().equals(befStr)) {
+				if (!candSolve(field, 0)) {
+					System.out.println(field);
+					return "問題に矛盾がある可能性があります。途中経過を返します。";
+				}
+				if (field.getStateDump().equals(befStr)) {
+					if (!candSolve(field, 1)) {
+						System.out.println(field);
+						return "問題に矛盾がある可能性があります。途中経過を返します。";
+					}
+					if (field.getStateDump().equals(befStr)) {
+						if (!candSolve(field, 2)) {
+							System.out.println(field);
+							return "問題に矛盾がある可能性があります。途中経過を返します。";
+						}
+						if (field.getStateDump().equals(befStr)) {
+							System.out.println(field);
+							return "解けませんでした。途中経過を返します。";
+						}
+					}
+				}
+			}
+		}
+		System.out.println(((System.nanoTime() - start) / 1000000) + "ms.");
+		System.out.println("難易度:" + (count * 10));
+		System.out.println(field);
+		return "解けました。推定難易度:"
+				+ Difficulty.getByCount(count * 10).toString();
+	}
+
+	/**
+	 * 仮置きして調べる
+	 * @param posSet
+	 */
+	private boolean candSolve(Field field, int recursive) {
+		for (int yIndex = 0; yIndex < field.getYLength(); yIndex++) {
+			for (int xIndex = 0; xIndex < field.getXLength(); xIndex++) {
+				if (field.numbersCand[yIndex][xIndex].size() != 1) {
+					count++;
+					for (Iterator<Integer> iterator = field.numbersCand[yIndex][xIndex].iterator(); iterator
+							.hasNext();) {
+						int oneCand = iterator.next();
+						Field virtual = new Field(field);
+						virtual.numbersCand[yIndex][xIndex].clear();
+						virtual.numbersCand[yIndex][xIndex].add(oneCand);
+						boolean arrowCand = virtual.solveAndCheck();
+						if (arrowCand && recursive > 0) {
+							arrowCand = candSolve(virtual, recursive - 1);
+						}
+						if (!arrowCand) {
+							iterator.remove();
+						}
+					}
+					if (field.numbersCand[yIndex][xIndex].size() == 0) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+}
